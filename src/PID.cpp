95c6c7ce611dd540c55b1cc648830a6a6db5b43e
Apostolos Georgiadis <apostolos.georgiadis@nfiniity.com>
2017-11-22 15:24:53 +0100

    pid first shot

---
+++ src/PID.cpp
@@ -0,0 +1,36 @@
+#include "PID.h"
+
+using namespace std;
+
+/*
+* TODO: Complete the PID class.
+*/
+
+PID::PID() {}
+
+PID::~PID() {}
+
+void PID::Init(double Kp, double Ki, double Kd){
+    this->Kp = Kp;
+    this->Ki = Ki;
+    this->Kd = Kd;
+    p_error = 0.0;
+    i_error = 0.0;
+    d_error = 0.0;
+    isFirst = true;
+}
+
+void PID::UpdateError(double cte) {
+    if (isFirst){
+        p_error = cte;
+        isFirst = false;
+    }
+    d_error = cte - p_error;
+    p_error = cte;
+    i_error += cte;
+}
+
+double PID::TotalError() {
+    return -Kp * p_error - Kd * d_error - Ki * i_error;
+}
+
